b180247f205062587b48b78809f534b237113e8a
Michael Raleigh <27874434+Anapsys@users.noreply.github.com>
2023-05-09 14:39:39 -0700

    0.1
    + added files

---
+++ manual-anti-alias.lua
@@ -0,0 +1,5 @@
+--[[
+Anapse manual anti-aliser for Aseprite
+Given selection, aliases on the borders of the foreground color WITH the selected secondary color.
+Useful for quickly applying this style to large areas in larger drawings.
+]]--
